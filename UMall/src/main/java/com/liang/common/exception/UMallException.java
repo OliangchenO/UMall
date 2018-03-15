226bbaf2c4e3edb707e6f19d59e7d169f66b383f
OliangchenO <liang_chen@aliyun.com>
2018-03-15 17:17:49 +0800

    Initial commit

---
+++ UMall/src/main/java/com/liang/common/exception/UMallException.java
@@ -0,0 +1,12 @@
+package com.liang.common.exception;
+
+/**
+ * @author Exrick
+ * @date 2017/8/24
+ */
+public class UMallException extends RuntimeException {
+
+    public UMallException(String msg){
+        super(msg);
+    }
+}
---
+++ UMall/src/main/java/com/liang/manager/dao/TbRoleMapper.java
@@ -0,0 +1,32 @@
+package com.liang.manager.dao;
+
+import com.liang.manager.pojo.TbRole;
+import com.liang.manager.pojo.TbRoleExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface TbRoleMapper {
+    long countByExample(TbRoleExample example);
+
+    int deleteByExample(TbRoleExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(TbRole record);
+
+    int insertSelective(TbRole record);
+
+    List<TbRole> selectByExample(TbRoleExample example);
+
+    TbRole selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") TbRole record, @Param("example") TbRoleExample example);
+
+    int updateByExample(@Param("record") TbRole record, @Param("example") TbRoleExample example);
+
+    int updateByPrimaryKeySelective(TbRole record);
+
+    int updateByPrimaryKey(TbRole record);
+
+    List<String> getUsedRoles(@Param("id") int id);
+}
